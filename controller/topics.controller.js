8e8cc5e477a3bec222843152828cc448d44dbde5
J-Mro <j.k.mroueh@gmail.com>
2026-02-04 16:08:17 +0000

    remove console.log lines from topics.controller layer

--- controller/topics.controller.js
+++ controller/topics.controller.js
@@ -4,8 +4,6 @@ const {
 
 exports.getAllTopics = (req, res) => {
   getAllTopicsService().then((topics) => {
-    console.log(topics, "<< topics");
-    console.log(topics.rows, " << topics rows");
     res.status(200).send(topics);
   });
 };
