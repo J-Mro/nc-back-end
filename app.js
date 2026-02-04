92268118c52307fe3414a0ed6affd588b761af6d
J-Mro <j.k.mroueh@gmail.com>
2026-02-04 16:02:59 +0000

    update app.js to include /api/users endpoint

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const topicsRouter = require("./routes/topics.routes");
 const articlesRouter = require("./routes/articles.router");
+const usersRouter = require("./routes/users.router");
 
 app.use(express.json());
 
 app.use("/api/topics", topicsRouter);
 app.use("/api/articles", articlesRouter);
+app.use("/api/users", usersRouter);
 module.exports = app;
