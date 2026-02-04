3573cd4d8a921ad61f2e03ebba553441bdd0242b
J-Mro <j.k.mroueh@gmail.com>
2026-02-04 15:38:44 +0000

    update app test file to iterate through .then result array, also adding passing tests for /api/articles endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,10 +20,29 @@ describe("/api/topics", () => {
       .expect(200)
       .then(({ body }) => {
         expect(Array.isArray(body)).toBe(true);
-        const sampleTopic = body[0];
-        expect(typeof sampleTopic.slug).toBe("string");
-        expect(typeof sampleTopic.description).toBe("string");
-        expect(typeof sampleTopic.img_url).toBe("string");
+        for (const topic of body) {
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
+          expect(typeof topic.img_url).toBe("string");
+        }
+      });
+  });
+});
+describe("/api/articles", () => {
+  test("GET:200 - responds with the correct articles array with additional comment_count column", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        for (const article of body) {
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+        }
       });
   });
 });
