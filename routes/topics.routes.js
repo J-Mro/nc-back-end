6faf7b795f912363629613a590b751c7f613b90e
J-Mro <j.k.mroueh@gmail.com>
2026-02-05 15:43:38 +0000

    add router.all functionality to handle invalid method requests for topics

--- routes/topics.routes.js
+++ routes/topics.routes.js
@@ -3,5 +3,8 @@ const { getAllTopics } = require("../controller/topics.controller");
 const router = express.Router();
 
 router.get("/", getAllTopics);
+router.all("/", (req, res, next) => {
+  res.status(405).send({ msg: "Invalid method" });
+});
 
 module.exports = router;
