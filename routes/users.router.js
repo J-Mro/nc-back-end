7620a65e97f89a3b7e975d9cd54bae6433368932
J-Mro <j.k.mroueh@gmail.com>
2026-02-06 12:28:09 +0000

    adding invalid method error to user.routes passing corresponding test

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { getAllUsers } = require("../controller/users.controller");
+const invalidMethodHandler = require("../errors/invalidMethodHandler");
 
 router.get("/", getAllUsers);
+router.all("/", invalidMethodHandler);
 module.exports = router;
