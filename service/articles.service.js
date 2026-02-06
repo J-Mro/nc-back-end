660262af1f018d4108624714d7752114841c647b
J-Mro <j.k.mroueh@gmail.com>
2026-02-06 08:56:26 +0000

    add throw new Error functionality to accomodate error handling for aticle id

--- service/articles.service.js
+++ service/articles.service.js
@@ -1,4 +1,17 @@
-const { fetchAllArticles } = require("../models/articles.model");
+const {
+  fetchAllArticles,
+  fetchArticleById,
+} = require("../models/articles.model");
+const NotFoundError = require("../errors/NotFoundError");
 exports.getAllArticles = () => {
   return fetchAllArticles();
+};
+exports.getArticleById = (article_id) => {
+  return fetchArticleById(article_id).then((article) => {
+    if (article === undefined) {
+      throw new NotFoundError("Article ID not found");
+    } else {
+      return article;
+    }
+  });
 };
