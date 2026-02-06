16d4482d92095fc17ba48215675270d2d4736967
J-Mro <j.k.mroueh@gmail.com>
2026-02-06 08:59:14 +0000

    add .catch block to accomodate error handling for article id

--- controller/articles.controller.js
+++ controller/articles.controller.js
@@ -1,8 +1,19 @@
 const {
   getAllArticles: getAllArticlesService,
+  getArticleById: getArticleByIdService,
 } = require("../service/articles.service");
 exports.getAllArticles = (req, res) => {
   getAllArticlesService().then((articles) => {
     res.status(200).send(articles);
   });
+};
+exports.getArticleById = (req, res) => {
+  const { article_id } = req.params;
+  getArticleByIdService(article_id)
+    .then((articles) => {
+      res.status(200).send(articles);
+    })
+    .catch((err) => {
+      res.status(404).send({ msg: err.message });
+    });
 };
