9d43a894d01df0d9e9a7bcb2cae63b77f8970ba4
J-Mro <j.k.mroueh@gmail.com>
2026-02-09 17:23:46 +0000

    add logic to handle non-numeric comment_id passed as argumetns

--- service/comments.service.js
+++ service/comments.service.js
@@ -3,13 +3,17 @@ const {
   checkCommentIdExists,
 } = require("../models/comments.model");
 const NotFoundError = require("../errors/NotFoundError");
+const BadRequestError = require("../errors/BadRequestError");
 exports.deleteCommentById = (comment_id) => {
-  return checkCommentIdExists(comment_id).then((result) => {
-    console.log(result);
-    if (result === true) {
-      return deleteCommentByIdModel(comment_id);
-    } else {
-      throw new NotFoundError("Comment not found");
-    }
-  });
+  if (!isNaN(comment_id)) {
+    return checkCommentIdExists(comment_id).then((result) => {
+      if (result === true) {
+        return deleteCommentByIdModel(comment_id);
+      } else {
+        throw new NotFoundError("Comment not found");
+      }
+    });
+  } else {
+    throw new BadRequestError("Invalid ID");
+  }
 };
