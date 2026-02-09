ee8cda0cf8ee735953476a418f27852487a12e6f
J-Mro <j.k.mroueh@gmail.com>
2026-02-09 17:22:16 +0000

    add method to handle errors when an invalid method is invoked

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { deleteCommentById } = require("../controller/comments.controller");
+const invalidMethodHandler = require("../errors/invalidMethodHandler");
 
-router.route("/:comment_id").delete(deleteCommentById);
+router
+  .route("/:comment_id")
+  .delete(deleteCommentById)
+  .all(invalidMethodHandler);
 module.exports = router;
