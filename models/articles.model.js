1ad3af881d104434f4c71ad78c5c8b665c13ce8a
J-Mro <j.k.mroueh@gmail.com>
2026-02-12 21:14:18 +0000

    update fetchAllArticles query to include dynamic order feature

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,18 +1,19 @@
 const db = require("../db/connection");
-exports.fetchAllArticles = () => {
-  return db
-    .query(
-      `
+const format = require("pg-format");
+exports.fetchAllArticles = (sort_by, order) => {
+  const queryStr = format(
+    `
         SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count
         FROM articles
         LEFT JOIN comments
         ON articles.article_id = comments.article_id
         GROUP BY articles.article_id
-        ORDER BY articles.created_at DESC;`,
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+        ORDER BY articles.%I ${order};`,
+    sort_by,
+  );
+  return db.query(queryStr).then(({ rows }) => {
+    return rows;
+  });
 };
 exports.fetchArticleById = (article_id) => {
   return db
