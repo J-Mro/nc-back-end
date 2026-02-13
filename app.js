99c5add81c3d787167650d2939a554a124c4abb7
J-Mro <j.k.mroueh@gmail.com>
2026-02-13 22:08:12 +0000

    refactor to create error-handling middleware chain

--- app.js
+++ app.js
@@ -21,10 +21,21 @@ app.all("/*path", (req, res, next) => {
 app.use((err, req, res, next) => {
   if (err instanceof NotFoundError) {
     res.status(404).send({ msg: err.message });
+  } else {
+    next(err);
   }
+});
+
+app.use((err, req, res, next) => {
   if (err instanceof BadRequestError) {
     res.status(400).send({ msg: err.message });
+  } else {
+    next(err);
   }
+});
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "server error" });
 });
 
 module.exports = app;
