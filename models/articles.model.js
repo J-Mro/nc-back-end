dd491c0ce9f36a8d99fc338a2b8011561d963508
J-Mro <j.k.mroueh@gmail.com>
2026-02-14 16:45:29 +0000

    add CAST() to query for fetchAllArticles to convert aggregate function result to integer

--- models/articles.model.js
+++ models/articles.model.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 const format = require("pg-format");
 exports.fetchAllArticles = (sort_by, order, topic) => {
   const queryStrJoin = `
-        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count
+        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.comment_id) AS int) AS comment_count
         FROM articles
         LEFT JOIN comments
         ON articles.article_id = comments.article_id `;
@@ -18,7 +18,7 @@ exports.fetchAllArticles = (sort_by, order, topic) => {
   } else {
     const queryStr = format(
       `
-        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count
+        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.comment_id) AS int) AS comment_count
         FROM articles
         LEFT JOIN comments
         ON articles.article_id = comments.article_id
